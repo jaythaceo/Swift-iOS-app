8d6a3c31c576e400ffa249c82f3a1a5a2533c5ae
jaythaceo <jaythaceo@gmail.com>
2015-02-16 19:54:09 -0500

    [chore] Added tip class and model

---
+++ TipCalculatorModel.swift
@@ -0,0 +1,9 @@
+//
+//  TipCalculatorModel.swift
+//  TipCalculator
+//
+//  Created by Jason  Brooks on 2/16/15.
+//  Copyright (c) 2015 Jason  Brooks. All rights reserved.
+//
+
+import Foundation
