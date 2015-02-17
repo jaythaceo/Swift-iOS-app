182a2a1df58357ab9b30343112a47adeb9112b3f
jaythaceo <jaythaceo@gmail.com>
2015-02-16 20:02:42 -0500

    [chore] Added views files

--- TipCalculatorModel.swift
+++ TipCalculatorModel.swift
@@ -7,3 +7,45 @@
 //
 
 import Foundation
+
+class TipCalculatorModel {
+    
+    // 2
+    var total: Double
+    var taxPct: Double
+    var subtotal: Double {
+        get {
+            return total / (taxPct + 1)
+        }
+    }
+    
+    // 3
+    init(total: Double, taxPct: Double) {
+        self.total = total
+        self.taxPct = taxPct
+        
+    }
+    
+    // 4
+    func calcTipWithTipPct(tipPct: Double) -> Double {
+        return subtotal * tipPct
+    }
+    
+    // 1
+    func returnPossibleTips() -> [Int: Double] {
+        
+        let possibleTipsInferred = [0.15, 0.18, 0.20]
+        let possibleTipsExplicit:[Double] = [0.15, 0.18, 0.20]
+        
+        // 2
+        var retval = [Int: Double]()
+        for possibleTip in possibleTipsInferred {
+            let intPct = Int(possibleTip*100)
+            // 3
+            retval[intPct] = calcTipWithTipPct(possibleTip)
+        }
+        return retval
+        
+    }
+    
+}
